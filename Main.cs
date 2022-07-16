e05aadf01b0a4657ad120f68a44488731a12bad8
stefnotch <brandmairstefan@gmail.com>
2022-07-16 20:39:17 +0200

    Monitor brightness flow plugin

---
+++ Main.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Flow.Launcher.Plugin;
+
+namespace MonitorBrightness
+{
+    public class Main : IPlugin, IResultUpdated, IPluginI18n
+    {
+        internal PluginInitContext Context = null!;
+
+        // Docs: https://www.flowlauncher.com/docs/#/API-Reference/Flow.Launcher.Plugin/IResultUpdated
+        public event ResultUpdatedEventHandler? ResultsUpdated;
+
+        private static readonly Regex DigitRegex = new Regex(@"^[ +-]*(\d+)[ +-]*$", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+        public List<Result> Query(Query query)
+        {
+            string? errorMessage = null;
+            var digitMatches = DigitRegex.Match(query.Search);
+
+            List<Monitors.BrightnessInfo>? currentBrightnesses = null;
+            int targetBrightness;
+            if(digitMatches.Success)
+            {
+                if(int.TryParse(digitMatches.Groups[0].Value, out int value)) 
+                {
+                    targetBrightness = value;
+                } 
+                else
+                {
+                    targetBrightness = 0;
+                    errorMessage ??= "Failed to parse the number";
+                }
+            } 
+            else
+            {
+                currentBrightnesses = GetBrightnessAtCursor();
+                if (currentBrightnesses == null)
+                {
+                    targetBrightness = 69;
+                    errorMessage ??= "Failed to get current brightness";
+                }
+                else
+                {
+                    targetBrightness = GetAverageBrightness(currentBrightnesses);
+                }
+            }
+
+            targetBrightness += query.Search.Count(v => v == '+');
+            targetBrightness += -1 * query.Search.Count(v => v == '-');
+
+            targetBrightness = Math.Clamp(targetBrightness, 0, 100);
+
+            string targetBrightnessString = errorMessage == null ? "" + targetBrightness : errorMessage;
+            string subtitle = currentBrightnesses != null ?
+                Context.API.GetTranslation("plugin_monitorbrightness_current_brightness") + ": " + 
+                string.Join(", ", currentBrightnesses.Select(v => v.CurrentValue))
+                : "";
+
+            var result = new Result
+            {
+                Title = $"{Context.API.GetTranslation("plugin_monitorbrightness_title")}: {targetBrightnessString}",
+                SubTitle = subtitle,
+                Action = c =>
+                {
+                    SetBrightnessAtCursor((v) =>
+                    {
+                        v.CurrentValue = (uint)Math.Clamp(targetBrightness, v.MinValue, v.MaxValue);
+                    });
+                    return true;
+                },
+                IcoPath = "Images/app.png"
+            };
+            return new List<Result> { result };
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            Context = context;
+
+            // TODO: Ask Flow devs "Do I ever need to call Context.API.RemoveGlobalKeyboardCallback()"? Either way, please clarify docs.
+            Context.API.RegisterGlobalKeyboardCallback(HandleKeyboardShortcut);
+        }
+
+        private bool HandleKeyboardShortcut(int keyevent, int vkcode, SpecialKeyState state)
+        {
+            // Docs: https://www.flowlauncher.com/docs/#/API-Reference/Flow.Launcher.Plugin/FlowLauncherGlobalKeyboardEventHandlerR
+
+            //Context.API.ChangeQuery(keyevent + "," + vkcode + "," + (state.CtrlPressed?"1":"0") + (state.WinPressed ? "1" : "0"));
+            const int KeydownEvent = 256;
+            const int ArrowUpVk = 38;
+            const int ArrowDownVk = 40;
+
+            if(keyevent == KeydownEvent && (vkcode == ArrowUpVk || vkcode == ArrowDownVk) && state.CtrlPressed && state.WinPressed)
+            {
+                int delta = vkcode == ArrowUpVk ? 20 : -20;
+                SetBrightnessAtCursor((v) =>
+                {
+                    v.CurrentValue = (uint)Math.Clamp((int)v.CurrentValue + delta, v.MinValue, v.MaxValue);
+                });
+
+                // TODO: Ask if there is an API to tell if a result is still visible/gives me a callback when a result goes away
+                /*
+                var currentBrightnesses = GetBrightnessAtCursor();
+                if (currentBrightnesses == null) return true;
+
+                int targetBrightness = GetAverageBrightness(currentBrightnesses);
+                targetBrightness += vkcode == ArrowUpVk ? 20 : -20;
+
+                Context.API.ChangeQuery(Context.CurrentPluginMetadata.ActionKeyword + " " + targetBrightness);
+                Context.API.ShowMainWindow();
+                */
+                return false;
+            }
+            return true;
+        }
+
+        private static int GetAverageBrightness(List<Monitors.BrightnessInfo> brightnesses)
+        {
+            return (int)brightnesses.Average(v => v.CurrentValue);
+        }
+
+        private List<Monitors.BrightnessInfo>? GetBrightnessAtCursor()
+        {
+            var monitor = Monitors.GetHMonitorAtCursor();
+            if(monitor == null)
+            {
+                LogException(new Exception("Cannot get monitor at cursor"));
+                return null;
+            }
+
+            var physicalMonitors = Monitors.GetMonitorsFrom(monitor);
+            if (physicalMonitors == null)
+            {
+                LogException(new Exception("Cannot get physical monitors"));
+                return null;
+            }
+            try
+            {
+                var brightnesses = Monitors.GetMonitorBrightnesses(physicalMonitors);
+                if (brightnesses.Count == 0)
+                {
+                    LogException(new Exception("Cannot get brightness"));
+                    return null;
+                }
+
+                return brightnesses;
+            } 
+            finally
+            {
+                Monitors.Destroy(physicalMonitors);
+            }
+        }
+
+        private void SetBrightnessAtCursor(Action<Monitors.BrightnessInfo> callback)
+        {
+            var monitor = Monitors.GetHMonitorAtCursor();
+            if (monitor == null)
+            {
+                LogException(new Exception("Cannot get monitor at cursor"));
+                return;
+            }
+
+            var physicalMonitors = Monitors.GetMonitorsFrom(monitor);
+            if (physicalMonitors == null)
+            {
+                LogException(new Exception("Cannot get physical monitors"));
+                return;
+            }
+            try
+            {
+                Monitors.UpdateMonitorBrightnesses(physicalMonitors, callback);
+            }
+            finally
+            {
+                Monitors.Destroy(physicalMonitors);
+            }
+        }
+
+        public string GetTranslatedPluginTitle()
+        {
+            return Context.API.GetTranslation("plugin_monitorbrightness_plugin_name");
+        }
+
+        public string GetTranslatedPluginDescription()
+        {
+            return Context.API.GetTranslation("plugin_monitorbrightness_plugin_description");
+        }
+
+        private void LogException(Exception exception)
+        {
+            Context.API.LogException("Main.cs", "MonitorBrightness.Flow", exception);
+        }
+    }
+}
---
+++ Monitors.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorBrightness
+{
+    internal class Monitors
+    {
+        public static HMonitor? GetHMonitorAtCursor()
+        {
+            if (!NativeMethods.GetCursorPos(out var cursor))
+            {
+                return null;
+            }
+            IntPtr monitorPtr = NativeMethods.MonitorFromPoint(cursor, NativeMethods.MonitorFromPointFlags.MONITOR_DEFAULTTONEAREST);
+            if (monitorPtr == IntPtr.Zero)
+            {
+                return null;
+            }
+            return new HMonitor(monitorPtr);
+        }
+
+        public class HMonitor
+        {
+            public HMonitor(IntPtr monitorPtr)
+            {
+                MonitorPtr = monitorPtr;
+            }
+
+            public IntPtr MonitorPtr { get; }
+        }
+
+        public static NativeMethods.PHYSICAL_MONITOR[]? GetMonitorsFrom(HMonitor hMonitor)
+        {
+            uint physicalMonitorsCount = 0;
+            if (!NativeMethods.GetNumberOfPhysicalMonitorsFromHMONITOR(hMonitor.MonitorPtr, ref physicalMonitorsCount))
+            {
+                return null;
+            }
+
+            var physicalMonitors = new NativeMethods.PHYSICAL_MONITOR[physicalMonitorsCount];
+            if (!NativeMethods.GetPhysicalMonitorsFromHMONITOR(hMonitor.MonitorPtr, physicalMonitorsCount, physicalMonitors))
+            {
+                return null;
+            }
+
+            return physicalMonitors;
+        }
+
+        public static List<BrightnessInfo> GetMonitorBrightnesses(NativeMethods.PHYSICAL_MONITOR[] physicalMonitors)
+        {
+            var result = new List<BrightnessInfo>();
+            foreach (var monitor in physicalMonitors)
+            {
+                uint minValue = 0, currentValue = 0, maxValue = 0;
+                if (!NativeMethods.GetMonitorBrightness(monitor.physicalMonitorPtr, ref minValue, ref currentValue, ref maxValue))
+                {
+                    continue;
+                }
+
+                result.Add(new BrightnessInfo()
+                {
+                    MinValue = minValue,
+                    MaxValue = maxValue,
+                    CurrentValue = currentValue,
+                });
+            }
+            return result;
+        }
+
+        public static void UpdateMonitorBrightnesses(NativeMethods.PHYSICAL_MONITOR[] physicalMonitors, Action<BrightnessInfo> callback)
+        {
+            var exceptions = new List<Exception>();
+            foreach (var monitor in physicalMonitors)
+            {
+                uint minValue = 0, currentValue = 0, maxValue = 0;
+                if (!NativeMethods.GetMonitorBrightness(monitor.physicalMonitorPtr, ref minValue, ref currentValue, ref maxValue))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    var monitorBrightness = new BrightnessInfo()
+                    {
+                        MinValue = minValue,
+                        MaxValue = maxValue,
+                        CurrentValue = currentValue,
+                    };
+                    callback(monitorBrightness);
+
+                    if (monitorBrightness.CurrentValue != currentValue)
+                    {
+                        uint newBrightness = Math.Clamp(monitorBrightness.CurrentValue, minValue, maxValue);
+
+                        if (!NativeMethods.SetMonitorBrightness(monitor.physicalMonitorPtr, newBrightness))
+                        {
+                            exceptions.Add(new Exception("Failed to set monitor brightness"));
+                        }
+                    }
+                } 
+                catch (Exception e)
+                {
+                    exceptions.Add(e);
+                }
+            }
+
+            if(exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+
+        public static void Destroy(NativeMethods.PHYSICAL_MONITOR[] physicalMonitors)
+        {
+            NativeMethods.DestroyPhysicalMonitors((uint)physicalMonitors.Length, ref physicalMonitors);
+        }
+
+        public class BrightnessInfo
+        {
+            public uint MinValue { get; init; }
+            public uint MaxValue { get; init; }
+            public uint CurrentValue { get; set; }
+        }
+    }
+}
